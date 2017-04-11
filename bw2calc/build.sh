ff35d1147fc905d2f896a16a8f0902bf0d226fc3
adrian <adrian.haas@ifu.baug.ethz.ch>
2017-04-11 09:22:49 +0200

    old bw2calc

---
+++ bw2calc/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#$PYTHON -m pip install stats_arrays
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
